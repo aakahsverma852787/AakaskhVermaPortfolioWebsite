2f6ec29ffba23835398b1591eb5b64e60b4e1d2d
Aakash Verma <aakashverma852787@gmail.com>
2025-10-04 15:23:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/AakaskhVermaPortfolioWebsite/", // 👈 Required for GitHub Pages subpath
   server: {
     host: "::",
     port: 8080,
